7041666e52747c58cf5b3b138272260da298df33
Bhupendra Kumar Sharma <99486803+Bhupendra308@users.noreply.github.com>
2024-06-21 09:45:53 +0530

    Update and rename trial.py to MYSQL_connectivity.py

--- MYSQL_connectivity.py
+++ MYSQL_connectivity.py
@@ -1,5 +1,5 @@
 import mysql.connector as t
-mydb = t.connect(host = "localhost",user = "root",passwd = "Bhupendra.2004")
+mydb = t.connect(host = "localhost",user = "root",passwd = "123")
 mycur = mydb.cursor()
 mycur.execute('show databases')
 for i in mycur:
